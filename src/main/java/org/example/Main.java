b364b507806742a4d5dae25e62bc05d30678e3bc
Jakekick99 <jakekallarackal@gmail.com>
2023-09-06 10:09:36 +0530

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,8 +10,7 @@ public class Main {
     private static final Logger logger = LogManager.getLogManager().getLogger(String.valueOf(Main.class));
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        logger.log
+        // IntelliJ IDEA suggests fixing it
         System.out.println("-----Welcome to Calculator-----\n" +
                 "Choose your operation\n" +
                 "1. Addition\n" +
